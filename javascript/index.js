f8babb7dd82af81370a398e7cdae6444504de047
leodvincci <lpenrose@depaul.edu>
2022-07-26 16:55:44 -0500

    updated bill layout, added fetch to pull json until API.

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,16 @@
 const billsLi = document.querySelector("#li-bills");
 const billsContainer = document.querySelector("#bills-container")
 
+const billModal = document.querySelector("#add-bill-modal");
+const billBtn = document.querySelector("#add-bill-btn");
+const body = document.querySelector("body");
+const billList = document.querySelector("#bill-list")
+const billData = ()=>fetch("./json/bills.json")
+    .then( x => {return x.json()})
+
+
+
+
 
 billsLi.addEventListener("click", ()=>{
     if(billsContainer.classList.contains("hidden")){
@@ -12,3 +22,54 @@ billsLi.addEventListener("click", ()=>{
 
     }
 } )
+
+
+
+
+
+billBtn.addEventListener("click", ()=>{
+        billModal.classList.remove("hidden");
+        console.log("I HIDE IT")
+
+} )
+
+window.addEventListener("keydown",(evt)=>{
+            if(evt.key ==="Escape"){
+                billModal.classList.add("hidden");
+                console.log("I HIDE IT")
+            }
+
+
+
+} )
+
+
+billData().then(x =>
+{
+    for(let i =0; i < x.length; i++){
+        const newDiv = document.createElement("div")
+        newDiv.classList.add("myBill")
+        newDiv.id = `b${x[i].id}`
+
+
+        billList.append(newDiv)
+
+        const newP = document.createElement("p")
+        newP.id = `biller-${x[i]}`
+        newP.textContent = x[i].payee
+        newDiv.append(newP)
+
+        const newP2 = document.createElement("p")
+        newP2.id = `bill-amount-${x[i]}`
+        newP2.textContent = x[i].amount
+        newDiv.append(newP2)
+
+        const newBtn = document.createElement("button");
+        newBtn.classList.add(`bill-btn`)
+        newBtn.classList.add(x[i].status)
+        newBtn.id = `bill-status-${x[i].id}`
+        newBtn.textContent = x[i].status;
+        newDiv.append(newBtn)
+
+    }
+})
